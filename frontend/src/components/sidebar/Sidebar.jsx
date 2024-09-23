054db4f0a9fe1985c90ada7543918072ba6ffef8
Bithika Roy <roybithika188@gmail.com>
2024-09-23 20:39:00 +0530

    updated

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -3,15 +3,21 @@ import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
-	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput />
-			<div className='divider px-3'></div>
-			<Conversations />
-			<LogoutButton />
-		</div>
-	);
+  return (
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
+      <SearchInput />
+      <div className='divider px-3'></div>
+      <Conversations />
+      <div className="relative group">
+        <LogoutButton />
+        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 bg-gray-800 text-white px-2 py-1 rounded text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+          Logout
+        </span>
+      </div>
+    </div>
+  );
 };
+
 export default Sidebar;
 
 // STARTER CODE FOR THIS FILE
